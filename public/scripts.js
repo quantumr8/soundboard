a34cfffa53e0eebbb70c66b6223347bdf7197988
Sam <sam@rosss.dev>
2023-10-26 03:29:21 -0500

    fix button highlights

--- public/scripts.js
+++ public/scripts.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
   const soundboard = document.getElementById('soundboard');
+  const uploadBtn = document.getElementById('upload-btn');
+  const popup = document.getElementById('upload-popup');
   const fileInput = document.getElementById('file-input');
+  const urlInput = document.getElementById('url-input');
 
-  fileInput.addEventListener('change', handleFileUpload);
+  uploadBtn.addEventListener('click', openPopup);
+  fileInput.addEventListener('change', upload);
+  urlInput.addEventListener('input', () => fileInput.value = ''); // Clear file input if URL is entered
 
   async function loadSounds() {
     try {
@@ -28,57 +33,39 @@ document.addEventListener('DOMContentLoaded', () => {
     const audio = new Audio(`/sounds/${file}`);
     audio.play();
     button.classList.add('playing');
-    audio.addEventListener('ended', () => {
-      button.classList.remove('playing');
-    });
-    audio.addEventListener('pause', () => {
-      button.classList.remove('playing');
-    });
-}
+    audio.addEventListener('ended', () => button.classList.remove('playing'));
+    audio.addEventListener('pause', () => button.classList.remove('playing'));
+  }
 
-
-  async function handleFileUpload(event) {
-    const files = Array.from(event.target.files);
+  async function upload() {
     const formData = new FormData();
-    files.forEach(file => formData.append('sounds', file));
+    if (fileInput.files.length > 0) {
+      Array.from(fileInput.files).forEach(file => formData.append('sounds', file));
+    } else if (urlInput.value) {
+      formData.append('url', urlInput.value);
+    } else {
+      alert('Please select a file or enter a URL');
+      return;
+    }
+
     try {
       await fetch('/upload', { method: 'POST', body: formData });
+      closePopup();
       loadSounds();
     } catch (error) {
-      console.error('Failed to upload files', error);
+      console.error('Failed to upload file', error);
     }
+  }
+
+  function openPopup() {
+    popup.style.display = 'block';
+  }
+
+  function closePopup() {
+    popup.style.display = 'none';
+    fileInput.value = ''; // Reset file input
+    urlInput.value = ''; // Reset URL input
   }
 
   loadSounds();
 });
-
-function openPopup() {
-  document.getElementById('upload-popup').style.display = 'block';
-}
-
-function closePopup() {
-  document.getElementById('upload-popup').style.display = 'none';
-}
-
-async function upload() {
-  const fileInput = document.getElementById('file-input');
-  const urlInput = document.getElementById('url-input');
-  
-  if (fileInput.files.length > 0) {
-    const formData = new FormData();
-    Array.from(fileInput.files).forEach(file => formData.append('sounds', file));
-    await fetch('/upload', { method: 'POST', body: formData });
-  } else if (urlInput.value) {
-    await fetch('/upload', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ url: urlInput.value }),
-    });
-  } else {
-    alert('Please select a file or enter a URL');
-    return;
-  }
-  
-  closePopup();
-  loadSounds();
-}
